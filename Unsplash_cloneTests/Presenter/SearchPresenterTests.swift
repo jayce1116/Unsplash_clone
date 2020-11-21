ed71539510dd8229d77a057d64b540521fd5f91e
jayce1116 <jaycesub@gmail.com>
2020-11-22 02:43:00 +0900

    Added presenter tests

---
+++ Unsplash_cloneTests/Presenter/SearchPresenterTests.swift
@@ -0,0 +1,23 @@
+//
+//  SearchPresenterTests.swift
+//  Unsplash_cloneTests
+//
+//  Created by jsshin on 2020/11/22.
+//  Copyright © 2020 jaycesub. All rights reserved.
+//
+
+import XCTest
+@testable import Unsplash_clone
+
+class SearchPresenterTests: XCTestCase {
+    
+    var presenter: SearchPresenter!
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+        presenter = nil
+    }
+
+}
